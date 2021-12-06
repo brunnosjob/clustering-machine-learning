f22242a85080bcd28c02de86d05fa1ca433c1b3b
Bruno Rodrigues <74928057+brunnosjob@users.noreply.github.com>
2021-12-06 14:00:01 -0300

    Update web_app_cluster_fmcg.py

--- web_app_cluster_fmcg.py
+++ web_app_cluster_fmcg.py
@@ -12,22 +12,74 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
-
-# In[ ]:
-
-
 #Criando interface inicial
-st.header('Bem-vindo (a) à web app de segmentação de cliente')
-st.subheader('Tecnologia machine learning aplicada ao interesse do marketing')
-st.markdown(' ')
-st.markdown(' ')
+#Menu
+st.sidebar.subheader('Projeto de portfólio de Ciência de Dados')
+st.sidebar.markdown('''Em breve, disponibilizo o artigo descrevendo o passo a passo do desenvolvimento do modelo.''')
+st.sidebar.title('Menu')
+pag = st.sidebar.selectbox('Selecione a página', ['Interagir com a inteligência', 'Sobre o projeto', 'Dashboard'])
 
-#Criando espaços de preenchimento
-cliente = st.text_input('Insira o nome do cliente:')
-renda = st.number_input('Insira a renda do cliente:', 0, 1000000, 0)
-idade = st.number_input('Insira a idade do cliente:', 18, 150, 18)
-educacao = st.selectbox('Selecione a escolaridade do cliente:', ['Ensino básico', 'Graduado (a)', 'Especializado (a)', 'Doutor (a)'])
-ocupacao = st.selectbox('Selecione a ocupação do cliente:',['Desempregado', 'Empregado', 'Empreendedor'])
-sexo = st.selectbox('Selecione o sexo do cliente:', ['Feminino', 'Masculino'])
-estado_civil = st.selectbox('Selecione o estado civil do cliente:', ['Solteiro', 'Em uma relação'])
-tamanho_cidade = st.selectbox('Selecione o tamanho da cidade em que o cliente reside:', ['Pequena', 'Média', 'Grande'])
+st.sidebar.markdown('Feito por : Bruno Rodrigues Carloto')
+
+#Disponibilizando as redes sociais
+st.sidebar.markdown("Redes Sociais :")
+st.sidebar.markdown("- [Linkedin](https://www.linkedin.com/in/bruno-rodrigues-carloto)")
+st.sidebar.markdown("- [Medium](https://br-cienciadedados.medium.com)")
+st.sidebar.markdown("- [Github](https://github.com/brunnosjob)")
+
+#Apresentação da tela de interação com o modelo
+if pag == 'Interagir com a inteligência':
+  st.markdown('*__Observação: para mais informações acerca do projeto, clique na seta no canto esquerdo superior da tela__* ')
+  st.header('Bem-vindo (a) à web app de segmentação de cliente')
+  st.subheader('Tecnologia machine learning aplicada ao interesse do marketing')
+  st.markdown(' ')
+  st.markdown(' ')
+
+  #Criando espaços de preenchimento
+  cliente = st.text_input('Insira o nome do cliente:')
+  renda = st.number_input('Insira a renda do cliente:', 0, 1000000, 0)
+  idade = st.number_input('Insira a idade do cliente:', 18, 150, 18)
+  educacao = st.selectbox('Selecione a escolaridade do cliente:', ['Ensino básico', 'Graduado (a)', 'Especializado (a)', 'Doutor (a)'])
+  ocupacao = st.selectbox('Selecione a ocupação do cliente:',['Desempregado', 'Empregado', 'Empreendedor'])
+  sexo = st.selectbox('Selecione o sexo do cliente:', ['Feminino', 'Masculino'])
+  estado_civil = st.selectbox('Selecione o estado civil do cliente:', ['Solteiro', 'Casado'])
+  tamanho_cidade = st.selectbox('Selecione o tamanho da cidade em que o cliente reside:', ['Pequena', 'Média', 'Grande'])
+  
+  #Convertendo inputs educação
+  if educacao == 'Ensino básico':
+    educacao_num = 0
+  elif educacao == 'Graduado (a)':
+    educacao_num = 1
+  elif educacao == 'Especializado (a)':
+    educacao_num = 2
+  elif educacao == 'Doutor (a)':
+    educacao_num = 3
+    
+  #Convertendo inputs ocupação
+  if ocupacao == 'Desempregado':
+    ocupacao_num = 0
+  elif ocupacao == 'Empregado':
+    ocupacao_num = 1
+  elif ocupacao == 'Empreendedor':
+    ocupacao_num = 2
+   
+    
+  #Convertendo inputs sexo
+   if sexo == 'Feminino':
+    sexo_num = 0
+  elif sexo == 'Masculino':
+    sexo_num = 1
+    
+  #Convertendo inputs estado civil
+  if estado_civil == 'Solteiro':
+    estado_civil_num = 0
+  elif estado_civil == 'Casado':
+    estado_civil_num = 1
+    
+  #Convertendo inputs tamanho da cidade
+  if tamanho_cidade == 'Cidade pequena':
+    tamanho_cidade_num = 0
+  elif tamanho_cidade == 'Cidade média':
+    tamanho_cidade_num = 1
+  elif tamanho_cidade == 'Cidade grande':
+    tamanho_cidade_num = 2
